e615447f9aeb88f22882516544219225c0691dea
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-06-29 09:30:13 +0000

    add bomb sys

--- js/main.js
+++ js/main.js
@@ -92,7 +92,23 @@ window.onload = function() {
 
             score += 10;
             scoreText.setText('Score: ' + score);
-
+        
+            if (stars.countActive(true) === 0)
+            {
+                stars.children.iterate(function (child) {
+        
+                    child.enableBody(true, child.x, 0, true, true);
+        
+                });
+        
+                var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        
+                var bomb = bombs.create(x, 16, 'bomb');
+                bomb.setBounce(1);
+                bomb.setCollideWorldBounds(true);
+                bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        
+            }
         }
 
         this.physics.add.overlap(player, stars, collectStar, null, this);
@@ -101,6 +117,24 @@ window.onload = function() {
         var scoreText;
 
         scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    
+        bombs = this.physics.add.group();
+
+        this.physics.add.collider(bombs, platforms);
+
+        this.physics.add.collider(player, bombs, hitBomb, null, this);
+        
+        function hitBomb (player, bomb)
+        {
+            this.physics.pause();
+
+            player.setTint(0xff0000);
+
+            player.anims.play('turn');
+
+            gameOver = true;
+        }
+
     }
     
     function update ()
