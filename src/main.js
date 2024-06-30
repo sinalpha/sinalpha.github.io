4d19ed76715d316c16d50ed62476518fca3bb3e2
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-06-30 13:37:12 +0000

    resize

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import Game from './scenes/game.js'
 import BootLoader from './scenes/bootLoader.js';
 
 const config = {
-    widht: 400,
-    height: 850,
+    
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_V,
+        widht: 400,
+        height: 850,
     },
     parent: "container",
     scene: [BootLoader, Splash, Game]
