0e6048cafe74503894203be344290f505ad6f2fc
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-06-30 10:12:39 +0000

    test setLoadEvents

--- src/scenes/bootLoader.js
+++ src/scenes/bootLoader.js
@@ -21,12 +21,29 @@ export default class BootLoader extends Phaser.Scene {
         this.progressBar = this.add.graphics();
     }
         
-
-    setLoadEvents(){
-        // this.load.on(
-        //     "progress",
-
-        // )
+    setLoadEvents() {
+        this.load.on(
+            "progress",
+            function (value) {
+                this.progressBar.clear();
+                this.progressBar.fillStyle(0x0088aa, 1);
+                this.progressBar.fillRect(
+                    this.cameras.main.width / 4,
+                    this.cameras.main.height / 2 - 16,
+                    (this.cameras.main.width / 2) * value,
+                    16
+                ) ;
+            },
+            this
+        );
+        
+        this.load.on(
+            "complete",
+            () => {
+                this.scene.start("splash");
+            },
+            this
+        );
     }
 
     
