b76eac34bef0c5f3fafaf3090aae223dcf2eeb84
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-06-30 14:45:56 +0000

    add uniguri

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -10,8 +10,12 @@ export default class Splash extends Phaser.Scene {
     }
 
     create(){
-
+        this.width = this.sys.game.config.width;
+        this.height = this.sys.game.config.height;
+        this.center_width = this.width / 2;
+        this.center_height = this.height / 2;
         this.cameras.main.setBackgroundColor(0x000000);
+        this.physics.add.sprite(640, 450, 'uniguri-splash')
 
         this.input.keyboard.on("keydown-SPACE", this.startGame, this);
     }
