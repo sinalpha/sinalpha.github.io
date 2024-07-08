9aeb32a294aa7e0118e68c1c65a90d02f7aa442b
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-08 08:37:28 +0000

    debug

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -10,8 +10,9 @@ export default class Menu extends Phaser.Scene{
     }
 
     create(){
+        console.log(this.parent);
         this.cameras.main.setBackgroundColor(0x111111);
-        // this.cameras.main.setViewport(this.parent.x, this.parent.y, 100, 100);
+        this.cameras.main.setViewport(this.parent.x, this.parent.y, 100, 100);
     }
 
 }
