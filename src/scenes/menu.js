64ba5986832a6c1a8940e950c3649633dce36e8f
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-08 07:34:04 +0000

    make half of the pre

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -12,7 +12,7 @@ export default class Menu extends Phaser.Scene{
 
     create(){
         this.cameras.main.setBackgroundColor(0x111111);
-        
+        this.cameras.main.setViewport(this.parent.x, this.parent.y, 100, 100);
     }
 
 }
