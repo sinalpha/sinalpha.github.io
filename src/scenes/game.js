3d531c12fd6bb2a13e49487f30333d7f662e94b2
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-13 05:32:05 +0000

    test

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -4,8 +4,6 @@ import Uniguri from "../gameObjects/uniguri.js"
 export default class Game extends Phaser.Scene {
     constructor(){
         super({ key : "game" });
-
-
     }
 
     preload(){
@@ -15,9 +13,6 @@ export default class Game extends Phaser.Scene {
     create(){
         this.cameras.main.setBackgroundColor(0x87ceeb);
         this.divideScreen();
-        this.physics.world.setBounds(
-            0, 0, this.bound.width, 100
-        );
 
         this.uniguri = new Uniguri(this, this.cameras.main.width / 2, 0);
     }
@@ -35,9 +30,12 @@ export default class Game extends Phaser.Scene {
         
         this.menuSize = this.bound;
         
-        //set game scene camera
+        //set physics bounds&game scene camera
         this.cameras.main.width = this.bound.width;
         this.cameras.main.height = this.bound.height;      
+        this.physics.world.setBounds(
+            0, 0, this.bound.width, 100
+        );
 
 
         //create menu
@@ -48,9 +46,9 @@ export default class Game extends Phaser.Scene {
             this.menuSize.height
         ).setInteractive().setOrigin(0);
     
-        this.menu = new Menu(this.menuZone);
+        // this.menu = new Menu(this.menuZone);
 
-        this.scene.add("menu", this.menu, true);
+        // this.scene.add("menu", this.menu, true);
     
     }
 }
