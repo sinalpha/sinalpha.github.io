f31b49eaa2a8381f750c7aed7cb1a1403cdcfe1f
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-13 06:27:18 +0000

    test

--- src/gameObjects/uniguri.js
+++ src/gameObjects/uniguri.js
@@ -3,7 +3,7 @@ export default class Uniguri extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y){
         super(scene, x, y);
 
-        // scene.add.existing(this);
+        scene.add.existing(this);
         // scene.physics.add.existing(this);
     }
 
