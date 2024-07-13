f2151e568cd08a6cab2f0d97616b5a68ee7099b2
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-13 07:28:57 +0000

    add Menu

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -12,6 +12,11 @@ export default class Game extends Phaser.Scene {
 
     create(){
         
+        this.bound = { 
+            height:this.cameras.main.height / 2,
+            width:this.cameras.main.width
+        };
+
         this.divideScreen();
         this.setWorld();
         this.addUniguri();
@@ -24,30 +29,9 @@ export default class Game extends Phaser.Scene {
     
     divideScreen(){
         
-        this.bound = { 
-                height:this.cameras.main.height / 2,
-                width:this.cameras.main.width
-            };
-            
-            this.menuSize = this.bound;
-            
         // set physics bounds&game scene camera
-        // this.cameras.main.width = this.bound.width;
-        // this.cameras.main.height = this.bound.height;      
-
-            
-            
-            //create menu
-            // this.menuZone = this.add.zone(
-                //     0,
-        //     this.menuSize.height,
-        //     this.menuSize.width,
-        //     this.menuSize.height
-        // ).setInteractive().setOrigin(0);
-        
-        // this.menu = new Menu(this.menuZone);
-
-        // this.scene.add("menu", this.menu, true);
+        this.cameras.main.width = this.bound.width;
+        this.cameras.main.height = this.bound.height;      
     
     }
     
@@ -56,6 +40,19 @@ export default class Game extends Phaser.Scene {
         this.physics.world.setBounds(
             0, 0, this.bound.width, this.bound.height
         );
+    }
+
+    createMenu(){
+        this.menuZone = this.add.zone(
+            0,
+            this.menuSize.height,
+            this.menuSize.width,
+            this.menuSize.height
+        ).setInteractive().setOrigin(0);
+        
+        this.menu = new Menu(this.menuZone);
+
+        this.scene.add("menu", this.menu, true);
     }
 
     setGameBackground(){
