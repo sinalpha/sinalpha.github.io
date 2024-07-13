c52de6acdb4996c321a9d60ecd4f2504b9b4890d
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-13 06:06:34 +0000

    test

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -11,17 +11,23 @@ export default class Game extends Phaser.Scene {
     }
 
     create(){
+        
         this.cameras.main.setBackgroundColor(0x87ceeb);
+        
         this.divideScreen();
+        this.addUniguri();
 
-        this.uniguri = new Uniguri(this, this.cameras.main.width / 2, 100);
-        this.physics.add.existing(this.uniguri, false);
-        this.uniguri.body.setCollideWorldBounds(true);
-        this.uniguri.body.setGravityY(3);
+
     }
 
     update(){
 
+    }
+
+    addUniguri(){
+        this.player = this.add.rectangle(400, 300, 64, 64, 0xffffff);
+        this.physics.add.existing(this.player, false);
+        this.player.body.setCollideWorldBounds(true);
     }
 
     divideScreen(){
@@ -31,23 +37,23 @@ export default class Game extends Phaser.Scene {
             width:this.cameras.main.width
         };
         
-        this.menuSize = this.bound;
+        // this.menuSize = this.bound;
         
-        //set physics bounds&game scene camera
-        // this.cameras.main.width = this.bound.width;
-        // this.cameras.main.height = this.bound.height;      
-        // this.physics.world.setBounds(
-        //     0, 0, this.bound.width, 100
-        // );
+        // set physics bounds&game scene camera
+        this.cameras.main.width = this.bound.width;
+        this.cameras.main.height = this.bound.height;      
+        this.physics.world.setBounds(
+            0, 0, this.bound.width, 100
+        );
 
 
         //create menu
-        this.menuZone = this.add.zone(
-            0,
-            this.menuSize.height,
-            this.menuSize.width,
-            this.menuSize.height
-        ).setInteractive().setOrigin(0);
+        // this.menuZone = this.add.zone(
+        //     0,
+        //     this.menuSize.height,
+        //     this.menuSize.width,
+        //     this.menuSize.height
+        // ).setInteractive().setOrigin(0);
     
         // this.menu = new Menu(this.menuZone);
 
