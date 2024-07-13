0eef021b1dc225f25a76815cb345c40907f6c8e8
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-13 09:05:21 +0000

    add button

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -12,11 +12,19 @@ export default class Menu extends Phaser.Scene {
 
         this.menuOrigin = {
             x:this.parent.x,
-            y:his.parent.y
+            y:this.parent.y
         };
+
+        this.buttonSize = {
+            width: this.parent.width / 5,
+            height: this.parent.height / 4,
+            blankWidth: this.parent.width / 25,
+            blankHeight: this.parent.height / 16
+        }
 
         this.setMenuBackground();
         this.setMenuCamera();
+        this.addButtons();
     }
 
     setMenuBackground(){
@@ -30,5 +38,34 @@ export default class Menu extends Phaser.Scene {
             this.parent.width, 
             this.parent.height
         );
+    }
+
+    addButtons(){
+        buttons = [
+            createButton('status','testbutton'),
+            createButton('clean','testbutton'),
+            createButton('sleep','testbutton'),
+            createButton('work','testbutton'),
+            createButton('shop','testbutton'),
+            createButton('food','testbutton'),
+            createButton('play','testbutton'),
+            createButton('concert','testbutton'),
+            createButton('lib','testbutton'),
+            createButton('badge','testbutton'),
+            createButton('credit','testbutton'),
+            createButton('config','testbutton'),
+        ];
+
+        for (let i = 0; i < 9; i++){
+            buttons[i].setPosition(
+                this.menuOrigin.x + this.buttonSize.blankWidth,
+                this.menuOrigin.y + this.buttonSize.blankHeight
+            );
+        }
+    }
+
+    createButton(name, img){
+        return  this.add.nineslice(this.buttonSize.width, this.buttonSize.height,
+            name, img);
     }
 }
