f6b40eb3b704a1f75f6ae1815329055c5a854ff8
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-14 05:35:20 +0000

    fix err

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -57,9 +57,9 @@ export default class Menu extends Phaser.Scene {
     }
     
     createButton(x, y, img, frame){
-        return new Phaser.GameObjects.NineSlice(this, x, y, img, frame,
-            this.buttonSize.width,
-            this.buttonSize.height
+        return new Phaser.GameObjects.NineSlice(this, x, y, img, 
+            width = this.buttonSize.width,
+            height = this.buttonSize.height
         );
     }
 }
