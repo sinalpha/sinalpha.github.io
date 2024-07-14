e65df502dca514692facb1c0dde7da51676352d3
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-14 09:38:03 +0000

    fix err

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -38,6 +38,6 @@ export default class Splash extends Phaser.Scene {
 
     setBackground(){
         this.add.image(this.width, this.height, 'background-splash');
-        this.uniguri = this.add.image(this.center.x,thiscenter.y,'uniguri-splash');
+        this.uniguri = this.add.image(this.center.x, this.center.y,'uniguri-splash');
     }
 }
