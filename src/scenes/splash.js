1ccf739fd9794a4ab952a5cd7456dd1c62013b97
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-14 09:24:48 +0000

    refac

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -6,33 +6,38 @@ export default class Splash extends Phaser.Scene {
     }
 
     preload(){
-
-    }
-
-    create(){
         const {x,y,width,height} = this.cameras.main;
         const center = {
             x: x+width/2, y: y+height/2
         }
+    }
 
-        //add sprite
-        this.background = this.add.tileSprite(x,y,width,height,'background-splash')
-            .setOrigin(0)
-            .setScrollFactor(0,1);
-        this.uniguri = this.add.image(center.x,center.y,'uniguri-splash');
+    create(){
 
-        //scene change trigger
-        this.input.once('pointerdown', () => {
-            new SceneEffect(this).simpleClose(this.startGame.bind(this));
-        });
+        this.setBackground();
+        this.startTrigger();
 
     }
 
     update(){
-        
+    }
+
+    startTrigger(){
+        this.input.once('pointerdown', () => {
+            new SceneEffect(this).simpleClose(this.startGame.bind(this));
+        });
     }
 
     startGame() {
         this.time.delayedCall(500, () => this.scene.start("game"), null, this);
+    }
+
+    setBackground(){
+        this.background = this.add.tileSprite(x,y,width,height,'background-splash')
+            .setOrigin(0)
+            .setScrollFactor(0,1);
+        this.background.width = width;
+        this.background.height = height;
+        this.uniguri = this.add.image(center.x,center.y,'uniguri-splash');
     }
 }
