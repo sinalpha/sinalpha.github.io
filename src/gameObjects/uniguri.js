366c70db72ec30d335978ec7882f913f8ab38aea
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-07-24 13:46:08 +0000

    test

--- src/gameObjects/uniguri.js
+++ src/gameObjects/uniguri.js
@@ -44,11 +44,9 @@ export default class Uniguri extends Phaser.Physics.Arcade.Sprite{
         this.setTexture("uniguri-sleep");
         this.setSize(80, 100);
 
-        this.do = "do";
-
         this.scene.sleepTimer = this.scene.time.addEvent({
             callback: ()=>{
-                console.log(this.do);
+                console.log(this.tiredness);
             },
             delay: 10000,
             loop: true
