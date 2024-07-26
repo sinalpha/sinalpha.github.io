a816c9b33247345f63adc0feeb69bd29e9cb567a
Sinalpha <hanhdhx65@gmail.com>
2024-07-26 10:13:22 +0000

    test

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -22,6 +22,7 @@ export default class Game extends Phaser.Scene {
     }
 
     create(){
+		this.scene.launch('status', this);
         this.adjustCamera();
         this.setWorld();
         this.setGameBackground();
