cad3f6cd25c9af392939d4175128b6e2c098b2eb
Sinalpha <hanhdhx65@gmail.com>
2024-07-27 10:35:45 +0000

    test

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -26,8 +26,6 @@ export default class Menu extends Phaser.Scene {
             blankWidth: this.gameSceneSize.width / 25,
             blankHeight: this.gameSceneSize.height / 16
         }
-		
-		console.log(this.cameras.main);
     }
 
     create(){
@@ -43,12 +41,12 @@ export default class Menu extends Phaser.Scene {
 
     setMenuCamera(){
 		this.scene.bringToTop();
-        // this.cameras.main.setViewport(
-        //     0, //the coner of top left
-        //     this.gameSceneSize.height, 
-        //     this.gameSceneSize.width, 
-        //     this.gameSceneSize.height
-        // );
+        this.cameras.main.setViewport(
+           	this.gameSceneSize.width,
+            this.gameSceneSize.height, 
+            0, //the coner of top left
+            this.gameSceneSize.height
+        );
     }
 
     
