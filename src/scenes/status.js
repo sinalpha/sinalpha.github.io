ab9fea146d49cf55a5e735f0c865b1af199cfa8c
Sinalpha <hanhdhx65@gmail.com>
2024-07-27 11:16:07 +0000

    fx err

--- src/scenes/status.js
+++ src/scenes/status.js
@@ -10,7 +10,7 @@ export default class Status extends Phaser.Scene {
 		// 	width: this.cameras.main.width,
 		// 	height: this.cameras.main.height / 2
 		// };
-		setSubScene(this, 0xffff00);
+		setSubScene(this, 0xffff00).bind(this);
 		
 		this.menuOrigin = {
             x:0,
