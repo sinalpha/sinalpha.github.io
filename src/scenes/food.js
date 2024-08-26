d1e0d1faa7e9c2d5583c205c7d7e659d18a5deba
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-08-26 13:48:45 +0000

    test

--- src/scenes/food.js
+++ src/scenes/food.js
@@ -40,7 +40,7 @@ export default class Food extends SubScene {
 
 			this.cameras.main.pan(
 				this.cameras.main.centerX, 
-				deltaY,
+				deltaY - (this.sceneSize.height / 2), //camera origin is center
 				50
 			);
 
