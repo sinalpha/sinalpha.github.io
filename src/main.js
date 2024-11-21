f87d52afef43eb20a81b8ae4e23f9c93ccea77dd
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-21 13:34:03 +0000

    FOL#001 fix err

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ const game = new Phaser.Game(config);
 
 window.addEventListener('resize', function(event) {
     
-    console.log("detected resize");
-    let myGameCanvas = document.getElementByTagName("canvas")[0];
+    let myGameCanvas = document.getElementsByTagName("canvas")[0];
 
     myGameCanvas.style.width = window.screen.width;
     myGameCanvas.style.height = window.screen.height;
