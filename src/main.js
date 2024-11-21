3ab9692415ca55c2b7ef91f96bf6ebbf9e9eb43e
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-21 13:30:26 +0000

    FOL#001 test changing canvas size

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ const game = new Phaser.Game(config);
 window.addEventListener('resize', function(event) {
     
     console.log("detected resize");
-    let myGameCanvas = getElementByTagName();
+    let myGameCanvas = document.getElementByTagName("canvas")[0];
+
+    myGameCanvas.style.width = window.screen.width;
+    myGameCanvas.style.height = window.screen.height;
+
+
+
 
 }, true);
