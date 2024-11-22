1c1725c175732d337a7dcf8ee2db2e42d872ea05
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-22 14:47:31 +0000

    PRJ#### reset project

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 ADD#n: 새로운 기능 n 개발  
 FOL#n: 기능 n 테스트  
 FIN#n: 기능 n 개발 완료  
-PRJ##: 프로젝트 관련 commit   
-REF##: 코드 리펙토링 
+PRJ##: 프로젝트 관련 commit
--- src/main.js
+++ src/main.js
@@ -1,54 +1,12 @@
-import Splash from './scenes/splash.js';
-import Game from './scenes/game.js';
-import BootLoader from './scenes/bootLoader.js';
-import Status from './scenes/status.js';
-import Menu from './scenes/menu.js';
-import Food from './scenes/food.js';
-
-/*
-
-    game Screen Size.
-
-*/
-const gameScreenSize = { width:window.innerWidth , height:window.innerHeight };
-
-/*
-
-    a parent of a canvas that is our game.
-
-*/
-const gameParent = "container";
-
-function addResizeEvnetListener(game){
-
-    let myGameCanvas = document.getElementsByTagName("canvas")[0];
-
-    window.addEventListener('resize', function(event) {
-        
-        myGameCanvas.width = window.screen.width;
-        myGameCanvas.height = window.screen.height;
-
-    }, true);
-
-}
-
-const config = {
-    width:gameScreenSize.width,
-    height:gameScreenSize.height,
-    type:Phaser.WEBGL,
-    parent: gameParent,
-    scene: [Splash, Game, BootLoader, Status, Menu, Food],
-    scale:{
-        mode:Phaser.Scale.FIT,
-        autoCenter:Phaser.Scale.CENTER_BOTH,
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    },
-    postBoot: addResizeEvnetListener
-}
+import {config} from './gameConfig.js'
 
 const game = new Phaser.Game(config);
+
+let myGameCanvas = document.getElementsByTagName("canvas")[0];
+
+window.addEventListener('resize', function(event) {
+    
+    myGameCanvas.width = window.screen.width;
+    myGameCanvas.height = window.screen.height;
+
+}, true);
