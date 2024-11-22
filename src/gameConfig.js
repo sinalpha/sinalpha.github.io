3d89fd2b030058a8863278cf23d4097eb81ce2be
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-22 14:53:11 +0000

    REF#### config

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -7,24 +7,27 @@ import Food from './scenes/food.js'
 
 const gameScreenSize = { width:window.innerWidth, height:window.innerHeight};
 
+const gameParent = "container";
+
 const gameScale = {
     mode:Phaser.Scale.FIT,
     autoCenter:Phaser.Scale.CENTER_BOTH,
 } 
 
+const gamePhysics = {
+    default: 'arcade',
+    
+    arcade: {
+        debug: false
+    }
+}
+
 export const config = {
     width:gameScreenSize.width,
     height:gameScreenSize.height,
     type:Phaser.WEBGL,
-    parent: 'container',
+    parent: gameParent,
     scene: [BootLoader, Splash, Game, Status, Menu, Food],
     scale:gameScale,
-    
-    physics:{
-        default: 'arcade',
-        
-        arcade: {
-            debug: false
-        }
-    }
+    physics:gamePhysics
 }
