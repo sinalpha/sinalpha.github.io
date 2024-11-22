fc3ecdb20ec7eb7de38205da75fb957f2b0c30c5
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-22 13:55:23 +0000

    REF#### fix err

--- src/main.js
+++ src/main.js
@@ -1,4 +1,50 @@
-import {config} from './gameConfig.js'
+import Splash from './scenes/splash.js';
+import Game from './scenes/game.js';
+import BootLoader from './scenes/bootLoader.js';
+import Status from './scenes/status.js';
+import Menu from './scenes/menu.js';
+import Food from './scenes/food.js';
+
+const gameScreenSize = { width:window.innerWidth , height:window.innerHeight };
+
+/*
+
+    a parent of a canvas that is our game.
+
+*/
+const gameParent = "container";
+
+/*
+
+    config scale.
+
+*/
+const gameScale = {
+    mode:Phaser.Scale.FIT,
+    autoCenter:Phaser.Scale.CENTER_BOTH,
+}
+
+/*
+
+    config physics.
+
+*/
+const gamePhysics = {
+    default: 'arcade',
+    arcade: {
+        debug: false
+    }
+}
+
+export const config = {
+    width:gameScreenSize.width,
+    height:gameScreenSize.height,
+    type:Phaser.WEBGL,
+    parent: gameParent,
+    scene: [Splash, Game, BootLoader, Status, Menu, Food],
+    scale:gameScale,
+    physics: gamePhysics
+}
 
 const game = new Phaser.Game(config);
 
