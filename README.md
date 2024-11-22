fa6e3263ee8b742a3cfead98d4c7e5c4eb77a2f8
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-22 13:31:38 +0000

    PRJ#### refactoring the project

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ADD#n: 새로운 기능 n 개발  
 FOL#n: 기능 n 테스트  
 FIN#n: 기능 n 개발 완료  
-PRJ##: 프로젝트 관련 commit
+PRJ##: 프로젝트 관련 commit   
 REF##: 코드 리펙토링 
--- src/gameConfig.js
+++ src/gameConfig.js
@@ -2,7 +2,7 @@ import { game_scenes } from './gameScenes.js'
 import { game_scale } from './gameScale.js'
 import { game_physics } from './gamePhysics.js'
 
-const gameScreenSize = { width:window.innerWidth ,height:window.innerHeight };
+const gameScreenSize = { width:window.innerWidth , height:window.innerHeight };
 const gameParent = "container";
 
 export const config = {
