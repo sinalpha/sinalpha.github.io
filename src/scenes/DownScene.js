445991b33f68d282fb1f02afb1a7a800c5efb1d3
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-23 04:36:46 +0000

    FOL#003 fix err

--- src/scenes/DownScene.js
+++ src/scenes/DownScene.js
@@ -6,13 +6,13 @@ export default class DownScene extends Phaser.Scene {
 	}
 	
 	setCameraViewPort(){
-		camera.setViewport(
+		this.cameras.main.setViewport(
 			this.scale.gameSize.height - this.downSceneScreenSize,
 			0, 
 			this.downSceneScreenSize.width,
 			this.downSceneScreenSize.height);
 
-		camera.setBackgroundColor(0x111111);
+		this.cameras.main.setBackgroundColor(0x111111);
 	}
 
 }
