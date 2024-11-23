0f22ca8e26f4b1321ed8c4440b5e95ae732c5f25
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-11-23 04:38:25 +0000

    FOL#003 fix err

--- src/scenes/UpScene.js
+++ src/scenes/UpScene.js
@@ -4,12 +4,12 @@ export default class UpScene extends Phaser.Scene{
 	}
 
 	setCameraViewPort(){
-		camera.setViewport(
+		this.cameras.main.setViewport(
 			0,
 			0, 
 			this.upSceneScreenSize.width,
 			this.upSceneScreenSize.height);
 
-		camera.setBackgroundColor(0x222222);
+		this.cameras.main.setBackgroundColor(0x222222);
 	}
 }
