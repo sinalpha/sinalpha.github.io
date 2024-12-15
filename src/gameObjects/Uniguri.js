8ee52eab6dfca4e8cf560a1151a7128cc2df01ee
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-12-15 05:56:10 +0000

    FOL#004 fix err

--- src/gameObjects/Uniguri.js
+++ src/gameObjects/Uniguri.js
@@ -27,7 +27,7 @@ export default class Uniguri extends Phaser.Physics.Arcade.Sprite{
                 from: GAME_FIGURES["MOVE_TIME"],
                 loop(){
                     console.log("test");
-                    uniguri.setVelocityY(10);
+                    this.scene.uniguri.setVelocityY(10);
                 }
             }
         ]).repeat().play();
